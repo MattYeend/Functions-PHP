853a6b2b2255e347530599f668cfb9f79931e725
Matt Yeend <mattyeend1@hotmail.co.uk>
2021-03-03 09:34:06 +0000

    Initial Commit

---
+++ Functions/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Functions</title>
+</head>
+<body>
+	<?php
+		function writeMsg(){
+			echo "Hello World!";
+		}
+		writeMsg();
+	?>
+</body>
+</html>
